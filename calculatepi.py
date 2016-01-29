e5656b7e0412aeeaa13a5f4484b392db9dd560d1
tesssny <teresasnyder@hanovernorwichschools.org>
2016-01-29 14:23:05 -0500

    Updated from Brython Server: 1/29/2016 2:23:05 PM

--- calculatepi.py
+++ calculatepi.py
@@ -1,7 +1,7 @@
 """
 calculatepi.py
 Author: Tess Snyder
-Credit: <list sources used, if any>
+Credit: 
 Assignment:
 
 Write and submit a Python program that computes an approximate value of π by calculating the following sum:
